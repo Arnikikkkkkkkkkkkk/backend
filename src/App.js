6e0b63ed81b4220b1e9f26b89db5aa5f0568173f
Arnikikkkkkkkkkkkk <ftnfhfgtm@gmail.com>
2023-04-28 20:03:00 +0500

    hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+const http = require("http");
+const HOST = "localhost";
+const PORT = 3003;
+const getUsers = require("./modules/users");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const requestListener = function (req, res) {
+  const url = new URL(req.url, "http://127.0.0.1");
+  const name = url.searchParams.get("hello");
+  const searchParams = url.searchParams;
 
-export default App;
+  if (url.searchParams.has("users")) {
+    res.statusMessage = "ok";
+    res.setHeader("Content-Type", "application/json");
+    res.write(getUsers());
+    res.end();
+    return;
+  }
+  if (name) {
+    res.statusCode = 200;
+    res.statusMessage = "ok";
+    res.setHeader("Content-Type", "text/html; charset=utf8");
+    const name = url.searchParams.get("hello");
+    res.write(`Hello, ${name}`);
+    res.end();
+    return;
+  }
+  if (name === "" || name === " ") {
+    res.statusCode = 400;
+    res.statusMessage = " Bad Request";
+    res.setHeader("Content-Type", "text/html; charset=utf8");
+    const name = url.searchParams.get("hello");
+    res.write(`Enter a name`);
+    res.end();
+    return;
+  }
+  if (![...searchParams].length) {
+    res.statusCode = 200;
+    res.statusMessage = "ok";
+    res.setHeader("Content-Type", "text/html; charset=utf8");
+    res.write("<h1>Hello, world!</h1>");
+    res.end();
+  }
+  res.statusCode = 500;
+  res.statusMessage = "Internal Server Error";
+  res.end();
+};
+const server = http.createServer(requestListener);
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
